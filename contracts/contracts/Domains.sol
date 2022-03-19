c88fe7baae18fec314019aef5fe5211b3c0ad8c8
pavanleton <pavankumarkpkp@gmail.com>
2022-03-19 16:47:19 +0530

    feat: register user domain om chain

--- contracts/contracts/Domains.sol
+++ contracts/contracts/Domains.sol
@@ -8,4 +8,15 @@ contract Domains {
     constructor() {
         console.log('This is my domian contract!');
     }
+
+    mapping(string => address) public domains;
+
+    function register(string calldata name) public {
+        domains[name] = msg.sender;
+        console.log("%s has registered a domain!", msg.sender);
+    }
+
+    function getAddress(string calldata name) public view returns (address) {
+        return domains[name];
+    }
 }
--- contracts/scripts/run.js
+++ contracts/scripts/run.js
@@ -1,8 +1,16 @@
 const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
     const domainContract = await domainContractFactory.deploy();
     await domainContract.deployed();
     console.log("Contract deployed to:", domainContract.address);
+    console.log("Contract deployed by:", owner.address);
+
+    const txn = await domainContract.register('pavan');
+    await txn.wait();
+
+    const domainOwner = await domainContract.getAddress('pavan');
+    console.log(`Domian owner ${domainOwner}`)
 };
 
 const runMain = async () => {
